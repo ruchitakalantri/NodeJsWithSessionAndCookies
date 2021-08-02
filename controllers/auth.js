a08bd14e289f422759c049a1164e568bb024f8e8
Ruchita Kalantri <rkalantri.kalantri@yahoo.com>
2021-08-02 10:49:37 +0530

    adding request driven login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,25 @@
 exports.getLogin = (req, res, next) => {
-        res.render('auth/login', {
-          path: '/login',
-          pageTitle: 'Login'
-        });
-      
+  // output req.get('Cookie') : loggedIn=true
+  console.log(req.get('Cookie').trim().split('=')[1]); // output : true
+  const isLoggedIn = req
+                      .get('Cookie')
+                      .trim()
+                      .split('=')[1]
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated : isLoggedIn
+  });
   };
+
+  exports.postLogin = (req, res, next) => {
+    // with this: req is within this block bcoz ag=fter redirect req is gone
+    // to store isLoggedIn data : we use cookies
+    //req.isLoggedIn = true;
+
+    // Cookies
+    res.setHeader('Set-Cookie' , 'loggedIn=true')
+
+    //Redirect to the given url 
+    res.redirect('/')
+};
